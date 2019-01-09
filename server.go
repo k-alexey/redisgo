3ca2332d931748663992f64c6e5d4677ccc7beac
Alexey Karpenko <k-alexey@users.noreply.github.com>
2019-01-10 00:53:30 +0300

    Implement server

--- server.go
+++ server.go
@@ -2,17 +2,29 @@ package main
 
 import (
 	"bufio"
+	"encoding/gob"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
+	"strings"
 )
 
-var port = flag.Int("port", 9090, "Server port")
-var mode = flag.String("mode", "disk", "Storage mode")
+var port = flag.Int("port", 9090, "The port for listening on")
+var mode = flag.String("mode", "disk", "The possible storage option")
 var help = flag.Bool("help", false, "Print help")
+
+type Message struct{
+	status string
+	message string
+}
+
+type Command struct{
+	line string
+	result chan Message
+}
 
 func init() {
 	flag.IntVar(port, "p", 9090, "Server port")
@@ -35,7 +47,7 @@ func main() {
 	log.Printf("Server is running on %d port\n", *port)
 	log.Println("Ready to accept connections")
 
-	commands := make(chan chan string)
+	commands := make(chan Command)
 
 	go storage(commands)
 
@@ -49,23 +61,65 @@ func main() {
 	}
 }
 
-// sends only single response
-func handle(commands chan chan string, conn net.Conn) {
+func handle(commands chan Command, conn net.Conn) {
 	defer log.Println("Disconnected", conn.RemoteAddr())
 	log.Println("Connected", conn.RemoteAddr())
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
 		ln := scanner.Text()
 		log.Println(conn.RemoteAddr(), ":", ln)
-		result := make(chan string)
-		commands <- result
-		io.WriteString(conn, "command: "+ln+", result: "+<-result+"\n")
+		command := Command{
+			line: ln,
+			result: make(chan Message)}
+		commands <- command
+		result := <- command.result
+		response := fmt.Sprintf("%d#%s%s\n", len(result.status), result.status, result.message)
+		io.WriteString(conn, response)
 	}
 	conn.Close()
 }
 
-func storage(commands chan chan string) {
-	for result := range commands {
-		result <- "It works!"
+func storage(commands chan Command) {
+	data := make(map[string]string)
+	file, err := os.Open("./data.dump")
+	if err == nil {
+				 decoder := gob.NewDecoder(file)
+				 err = decoder.Decode(&data)
+	}
+	file.Close()
+	log.Printf("Storage: %v", data)
+	for command := range commands {
+		fields := []string{"", "", ""}
+		for i, v := range strings.SplitN(command.line, " ", 3) {
+			fields[i] = v
+		}
+		result := Message{"", ""}
+		if fields[1] == ""{
+			result = Message{"ERR", "key required"}
+		} else {
+			switch strings.ToUpper(fields[0]){
+			case "SET":
+				data[fields[1]] = fields[2]
+				file, err := os.Create("./data.dump")
+				if err == nil {
+							 encoder := gob.NewEncoder(file)
+							 encoder.Encode(data)
+				}
+				file.Close()
+				result = Message{"OK",""}
+			case "GET":
+				if v, ok := data[fields[1]]; ok {
+					result = Message{"OK", v}
+				}else{
+					result = Message{"ERR", "missing value"}
+				}
+			case "DEL":
+				delete(data, fields[1])
+				result = Message{"OK", ""}
+			default:
+				result = Message{"ERR", "unknown command"}
+			}
+		}
+		command.result <- result
 	}
 }
